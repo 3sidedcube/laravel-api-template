7ed1528427870d1016ec70c34b8583eb86fb962c
Ben Sherred <ben@sherred.co.uk>
2022-02-10 09:48:12 +0000

    Update gitignore files, force schema, prevent lazy loading, unguard models

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use ThreeSidedCube\LaravelApiErrors\Exceptions\ApiErrorException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -13,7 +14,7 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
+        ApiErrorException::class,
     ];
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment(['production', 'staging', 'test', 'development', 'dev'])) {
+            URL::forceScheme('https');
+        }
+
+        Model::unguard();
+        Model::preventLazyLoading(! $this->app->isProduction());
     }
 }
