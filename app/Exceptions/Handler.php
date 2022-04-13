473759f274381353528241c8d6cd342b3f8e83ef
Ben Sherred <ben@sherred.co.uk>
2022-04-13 11:20:09 +0100

    Update Laravel to v9.8.1 (#2)
    * Update Laravel to v9.8.1
    
    * Fix linting

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -17,10 +17,18 @@ use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use ThreeSidedCube\LaravelApiErrors\Exceptions\ApiErrorException;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
@@ -35,7 +43,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<Throwable>>
+     * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
         ApiErrorException::class,
